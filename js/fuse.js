647402df10aab0fdf28a56d952bd88f39ad8f696
henriquecoan-code <henriquecoan@hotmail.com>
2025-06-20 10:42:49 -0300

    Produto/buscar
    Adicionado subcategoria, dcb e laboratorio.
    feito a integração ao buscar
    adicionado página para visualização dos produtos ao cliente

---
+++ js/fuse.js
@@ -0,0 +1,4 @@
+// Fuse.js v6.6.2 - https://fusejs.io/
+// Minified version (trecho inicial para uso local, pode ser substituído por CDN se preferir)
+// ... (coloque aqui o conteúdo minificado de Fuse.js caso queira local)
+// Para este exemplo, usaremos CDN no index.html
