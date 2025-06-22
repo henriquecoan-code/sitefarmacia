1b725bd78949bb285a7e3cce442cc87dc799eada
henriquecoan-code <henriquecoan@hotmail.com>
2025-06-22 11:55:58 -0300

    buscar2

--- js/header-search-integration.js
+++ js/header-search-integration.js
@@ -25,26 +25,6 @@
   setTimeout(attachHeaderSearchListener, 1000);
 })();
 
-// Limpa a busca ao clicar no menu lateral esquerdo (funciona mesmo se aside já estiver no DOM)
-(function() {
-  function clearSearchAndTrigger() {
-    var inputs = document.querySelectorAll('#header-search-input');
-    inputs.forEach(function(input) { input.value = ''; });
-    // Dispara busca em branco
-    window.dispatchEvent(new CustomEvent('headerSearch', { detail: '' }));
-  }
-  function attachAsideMenuListeners() {
-    var asideLinks = document.querySelectorAll('aside nav a[data-categoria], #aside-todos');
-    asideLinks.forEach(function(link) {
-      link.removeEventListener('click', clearSearchAndTrigger);
-      link.addEventListener('click', clearSearchAndTrigger);
-    });
-  }
-  document.addEventListener('DOMContentLoaded', attachAsideMenuListeners);
-  document.addEventListener('headerLoaded', attachAsideMenuListeners);
-  setTimeout(attachAsideMenuListeners, 1000);
-})();
-
 // No menu mobile, ao clicar em links de categoria, redireciona para index.html se não estiver nela
 (function() {
   function handleMobileMenuLink(e) {
